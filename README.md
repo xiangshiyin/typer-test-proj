9032becb28a8d49d9fcfe5fc203b663d76ef7438
Xiangshi Yin <piupiuyin@gmail.com>
2024-09-01 17:55:04 -0400

    advanced setup

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # typer-test-proj
 Play with Tyler for cli tool building
 
+## Package installation
+Run `poetry install`
+
 ## Commands to test
-### `python main.py hello`
-### `python main.py goodbye`
+### `typer-test-proj hello`
+### `typer-test-proj goodbye`
 
 ## Reference
 - `Typer` documentations [[link](https://typer.tiangolo.com/#run-the-code)]
-- 
+- Python types intro [[link](https://fastapi.tiangolo.com/python-types/)]
--- typer_test_proj/main.py
+++ typer_test_proj/main.py
@@ -1,17 +1,21 @@
 import typer
+from typing import Optional
 
 app = typer.Typer()
+
 
 @app.command()
 def hello(name: str):
     print(f"Hello {name}")
 
+
 @app.command()
-def goodbye(name: str, formal: bool = False):
+def goodbye(name: str, formal: bool = False, intro: Optional[str] = None):
     if formal:
         print(f"Goodbye Ms. {name}. Have a good day.")
     else:
         print(f"Bye {name}")
+
 
 if __name__ == "__main__":
     app()

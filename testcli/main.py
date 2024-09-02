3b42cf51df2c66b16cb30ee365e6b85e8a24d4f1
Xiangshi Yin <piupiuyin@gmail.com>
2024-09-02 16:44:41 -0400

    cmd options

--- testcli/main.py
+++ testcli/main.py
@@ -10,11 +10,18 @@ def hello(name: str):
 
 
 @app.command()
-def goodbye(name: str, formal: bool = False, intro: Optional[str] = None):
+def goodbye(
+    first_name: str,
+    formal: bool = False,
+    last_name: str = None,
+    intro: Optional[str] = None,
+):
+    full_name = f"{first_name} {last_name}" if last_name else f"{first_name}"
+    print(full_name)
     if formal:
-        print(f"Goodbye Ms. {name}. Have a good day.")
+        print(f"Goodbye Ms. {full_name}. Have a good day.")
     else:
-        print(f"Bye {name}")
+        print(f"Bye {full_name}")
 
 
 if __name__ == "__main__":
